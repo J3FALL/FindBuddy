32f70d06dd215c3702279aa2817bddc013e707a1
Ilnur Khafizov <ilnurkhafizoff@yandex.ru>
2016-11-19 01:12:47 +0300

    Add CayegoryDto

---
+++ src/main/java/org/edu/model/dto/CategoryDto.java
@@ -0,0 +1,35 @@
+package org.edu.model.dto;
+
+public class CategoryDto {
+
+    private long id;
+    private String name;
+    private String color;
+
+    public CategoryDto() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
