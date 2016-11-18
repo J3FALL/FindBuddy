a7c4e828508a6eed16981a3d959702d86c8cfc53
Pavel Vychuzhanin <crisp.coad@gmail.com>
2016-11-19 01:47:19 +0300

    Add MeetingDto

---
+++ src/main/java/org/edu/model/dto/MeetingDto.java
@@ -0,0 +1,53 @@
+package org.edu.model.dto;
+
+/**
+ * Created by Pavel on 19.11.2016.
+ */
+public class MeetingDto {
+
+    private long id;
+    private String title;
+    private String description;
+    private long longitude;
+    private long latitude;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(long latitude) {
+        this.latitude = latitude;
+    }
+}
