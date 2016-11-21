89552a856493e460143119ed3e40c48e35b79f1f
Ilnur Khafizov <ilnurkhafizoff@yandex.ru>
2016-11-21 16:00:47 +0300

    Add Category update, delete API

--- src/main/java/org/edu/controller/CategoryController.java
+++ src/main/java/org/edu/controller/CategoryController.java
@@ -9,6 +9,7 @@ import org.edu.util.GenericResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,12 +43,27 @@ public class CategoryController {
         return new ResponseEntity<>(Converter.convert(categoryService.getAllCategories(), CategoryDto.class), HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<GenericResponse> updateCategory(@RequestBody CategoryDto categoryDto, Principal principal, @PathVariable("id") long id) {
+        categoryDto.setId(id);
+        categoryService.updateCategory(Converter.convert(categoryDto, Category.class), principal);
+        return new ResponseEntity<>(new GenericResponse("Successful."), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<GenericResponse> deleteCategory(@PathVariable("id") long id, Principal principal) {
+        boolean isSuccess = categoryService.removeCategory(id, principal);
+        if (isSuccess)
+            return new ResponseEntity<>(new GenericResponse("Successful."), HttpStatus.OK);
+        else return new ResponseEntity<>(new GenericResponse("Fail."), HttpStatus.BAD_REQUEST);
+    }
+
     @RequestMapping(value = "/subscribe", method = RequestMethod.POST)
     public ResponseEntity<GenericResponse> subscribeUser(@RequestBody CategoryDto[] categoryDtos, Principal principal) {
         if (principal == null)
             return new ResponseEntity<>(new GenericResponse("Please login"), HttpStatus.BAD_REQUEST);
         List<CategoryDto> categories = new ArrayList<>(Arrays.asList(categoryDtos));
-        userService.subscribeCategories(Converter.convert(categories, Category.class), principal);
+        categoryService.subscribeCategories(Converter.convert(categories, Category.class), principal);
         return new ResponseEntity<>(new GenericResponse("Successful."), HttpStatus.OK);
     }
 }
