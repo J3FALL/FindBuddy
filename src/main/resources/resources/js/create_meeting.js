1599f3f40e51dc1fdded08d535c114ba8ccf0f6c
Pavel Vychuzhanin <crisp.coad@gmail.com>
2016-11-27 23:33:42 +0300

    Add post method for create_meeting.js
    Get all fields from form, create json and post to /meetings.

---
+++ src/main/resources/resources/js/create_meeting.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Pavel on 27.11.2016.
+ */
+$("#publish-button").click(function () {
+
+    var values = [];
+
+    //get fields with values from html-form
+    $("#create-meeting-form").each(function () {
+        values = $(this).serializeArray();
+        event.preventDefault();
+    });
+    //get date-value pair
+    var result = $.grep(values, function (e) {
+        return e.name == "date";
+    });
+    var d = new Date(result[0].value);
+    var date = d.toISOString().slice(0, 10);
+
+    //get time-value pair
+    result = $.grep(values, function (e) {
+        return e.name == "time";
+    })
+    var time = result[0].value;
+
+    //remove date & time objects from values
+    values = values.filter(function( obj ) {
+        return obj.name !== 'date' && obj.name !== 'time';
+    });
+
+    //add startDate field to json
+    var startDate = date + "T" + time + ":00";
+    values.push({name: 'start_date', value: startDate});
+
+    var array = {};
+    //convert array of object to object
+    for (var i=0; i<values.length; i++) {
+        array[values[i].name] = values[i].value;
+    }
+    $.ajax({
+                type: 'POST',
+                url: '/meetings',
+                contentType: "application/json",
+                data: JSON.stringify(array),
+           }).done(function (result) {
+        console.log(result);
+        window.location.replace("/");
+    }).fail(function (result) {
+        console.log(result);
+        console.log("fail");
+    });
+});
