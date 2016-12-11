d7b0ce4ae68658ff1263089398ed83e0937c51c9
Ilnur Khafizov <ilnurkhafizoff@yandex.ru>
2016-12-11 12:04:57 +0300

    Add sendComment function

---
+++ src/main/resources/resources/js/send_comment.js
@@ -0,0 +1,34 @@
+$(".send-comment").click(function () {
+    var meetingId = $(".send-comment").data('meeting');
+    var text = $("#new-comment-text").val();
+    var newComment = {
+        meetingId: meetingId,
+        text: text
+    };
+    $.ajax({
+               url: "/comments",
+               contentType: "application/json",
+               method: "POST",
+               data: JSON.stringify(newComment)
+           })
+        .done(function (data) {
+            drawNewComment(data);
+            $("#new-comment-text").val('');
+        })
+});
+
+function drawNewComment(newComment) {
+    var source = $('#comment-template').html();
+    var template = Handlebars.compile(source);
+    console.log(moment(newComment.date));
+    var date = moment(newComment.date).locale("ru").utcOffset(0, true).format("DD MMM HH:mm");
+    var context = {
+        authorLink: "/user/" + newComment.authorId,
+        authorPhoto: "/images/" + newComment.authorPhoto,
+        authorName: newComment.authorName,
+        commentText: newComment.text,
+        commentDate: date
+    };
+    var html = template(context);
+    $(html).hide().appendTo(".comment-container").slideDown("fast");
+}
